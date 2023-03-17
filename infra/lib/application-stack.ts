6afb41d6acc9fec7382a6cd1d6bed55ab1f7b81b
John Sherlock <john.sherlock@gmail.com>
2023-03-17 00:24:32 +0000

    Update application stack. Update UI to hit APIGW.

--- infra/lib/application-stack.ts
+++ infra/lib/application-stack.ts
@@ -1,27 +1,35 @@
+import * as path from 'path';
 import * as amplify from '@aws-cdk/aws-amplify-alpha';
 import * as cdk from 'aws-cdk-lib';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction, SourceMapMode } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 import { Construct } from 'constructs';
 
 export class ApplicationStack extends cdk.Stack {
 
   readonly amplifyApp: amplify.App;
+  readonly restApi: apigateway.RestApi;
   readonly pvProxyLambda: lambda.Function;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     this.amplifyApp = this.createAmplifyApp();
-    this.pvProxyLambda = this.createPvProxyLambda();
-
+    this.restApi = this.createRestApi();
+    this.pvProxyLambda = this.createPvMinuteDataHandler();
   }
 
-  private createPvProxyLambda(): lambda.Function {
-    return new NodejsFunction(this, 'MyEnergiProxy', {
+  private createPvMinuteDataHandler(): lambda.Function {
+
+    const modulePath = path.resolve(process.cwd());
+    const lambdaPath = path.join(modulePath, '../pv-proxy/src/myEnergiProxy.ts');
+
+    const myEnergiProxyFunction = new NodejsFunction(this, 'MyEnergiProxy', {
       functionName: 'MyEnergiProxy',
-      entry: __dirname + '../../pv-proxy/src/myEnergiProxy.ts',
+      entry: lambdaPath,
       bundling: {
         externalModules: [],
         minify: false,
@@ -29,15 +37,42 @@ export class ApplicationStack extends cdk.Stack {
         sourceMapMode: SourceMapMode.INLINE,
         sourcesContent: true,
       },
-      environment: {
-        MYENERGI_USERNAME: cdk.SecretValue.secretsManager('pvm', {
-          jsonField: 'myenergi-username',
-        }).toString(),
-        MYENERGI_PASSWORD: cdk.SecretValue.secretsManager('pvm', {
-          jsonField: 'myenergi-password',
-        }).toString(),
+    });
+
+    const myEnergiSecret = secretsmanager.Secret.fromSecretNameV2(this, 'pvmSecret', 'pvm');
+
+    myEnergiProxyFunction.addEnvironment('MYENERGI_USERNAME', `{{resolve:secretsmanager:${myEnergiSecret.secretArn}:SecretString:myenergi-username}}`);
+    myEnergiProxyFunction.addEnvironment('MYENERGI_PASSWORD', `{{resolve:secretsmanager:${myEnergiSecret.secretArn}:SecretString:myenergi-password}}`);
+
+    const pvMinuteDataResource = this.restApi.root.addResource('minute-data');
+    pvMinuteDataResource.addMethod('GET', new apigateway.LambdaIntegration(myEnergiProxyFunction));
+
+    return myEnergiProxyFunction;
+  }
+
+  private createRestApi(): apigateway.RestApi {
+
+    const api = new apigateway.RestApi(this, 'PV Manager API');
+
+    // Configure the default path to return a 404 response
+    const defaultIntegration = new apigateway.MockIntegration({
+      integrationResponses: [
+        {
+          statusCode: '200',
+          responseTemplates: {
+            'application/json': '{"message": "PV Manager API"}',
+          },
+        },
+      ],
+      passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
+      requestTemplates: {
+        'application/json': '{"statusCode": 404}',
       },
     });
+
+    api.root.addMethod('ANY', defaultIntegration);
+
+    return api;
   }
 
   private createAmplifyApp(): amplify.App {
