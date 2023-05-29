6ca1e4e1e14085769bf0068535cb85a6ee5ed639
jsherlockppb <john.sherlock@ppb.com>
2023-05-29 19:14:46 +0100

    Another hella refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './app';
-import reportWebVitals from './report-web-vitals';
+import './app/index.css';
+import App from './app/app';
+import reportWebVitals from './app/report-web-vitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
